d38475a2d3e3a4d3499c163c658a60cd0584ab01
brayangomez08 <gomezmancobrayanalexander@gmail.com>
2022-02-20 17:28:16 -0500

    Added home routing

--- src/app/ui/blocks/home-content/home-content.component.ts
+++ src/app/ui/blocks/home-content/home-content.component.ts
@@ -1,4 +1,5 @@
 import { Component, ChangeDetectionStrategy, ViewEncapsulation } from '@angular/core';
+import { Router } from '@angular/router';
 @Component({
   selector: 'mr-home-content',
   templateUrl: './home-content.component.html',
@@ -6,4 +7,10 @@ import { Component, ChangeDetectionStrategy, ViewEncapsulation } from '@angular/
   changeDetection: ChangeDetectionStrategy.OnPush,
   encapsulation: ViewEncapsulation.None,
 })
-export class HomeContentComponent { }
+export class HomeContentComponent { 
+  constructor(private router: Router) { }
+
+  navigateToChannel(url: string): void {
+    this.router.navigateByUrl(url);
+  }
+}
