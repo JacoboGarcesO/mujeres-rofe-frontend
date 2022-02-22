2fcd1b9892fb621bfe708997a24c5ed58e61c50a
JacoboGarcesO <jacobogarcesoquendo@gmail.com>
2022-02-21 21:57:23 -0500

    Show admin when user is common was fixed

--- src/app/ui/blocks/aside-menu/aside-menu.component.ts
+++ src/app/ui/blocks/aside-menu/aside-menu.component.ts
@@ -1,7 +1,8 @@
-import { Component, ChangeDetectionStrategy, Input, EventEmitter, Output, OnInit } from '@angular/core';
+import { Component, ChangeDetectionStrategy, Input, EventEmitter, Output } from '@angular/core';
+import { ChannelEnum } from 'src/app/core/enums/channel.enum';
+import { fromRolEnum } from 'src/app/core/enums/rols.enum';
 import { ChannelModel } from 'src/app/core/models/channel.model';
 import { CurrentUserModel } from 'src/app/core/models/current-user.model';
-import { AppState } from '../../../core/state/app.state';
 
 @Component({
   selector: 'mr-aside-menu',
@@ -9,21 +10,17 @@ import { AppState } from '../../../core/state/app.state';
   styleUrls: ['./aside-menu.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AsideMenuComponent implements OnInit {
+export class AsideMenuComponent {
   @Input() isVisible = false;
   @Input() channels: ChannelModel[];
   @Input() channel: ChannelModel;
-  @Input() currentUser: CurrentUserModel | undefined;
+  @Input() currentUser: CurrentUserModel;
   @Output() clickedLogout: EventEmitter<void> = new EventEmitter();
 
-  constructor(
-    private appState: AppState,
-  ) { }
-
-  ngOnInit(): void {
-    this.appState.users.currentUser.$().subscribe((user) => {     
-      if(user?.rol === 'user') this.channels.pop(); 
-    });
+  get channelsFiltered(): ChannelModel[] {
+    return fromRolEnum(this.currentUser.rol) === 'user' 
+      ? this.channels.filter((channel) => channel.type !== ChannelEnum.admin) 
+      : this.channels;
   }
 
   handleLogout(): void {
