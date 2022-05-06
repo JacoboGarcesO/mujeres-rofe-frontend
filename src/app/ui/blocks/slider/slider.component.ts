fdf25f6b910b28a9786e78ed356d0f56a1b4d12e
JacoboGarcesO <jacobogarcesoquendo@gmail.com>
2022-05-06 15:53:28 -0500

    Redirect notice

--- src/app/ui/blocks/slider/slider.component.ts
+++ src/app/ui/blocks/slider/slider.component.ts
@@ -1,4 +1,5 @@
 import { Component, ChangeDetectionStrategy, Input, ViewEncapsulation } from '@angular/core';
+import { Router } from '@angular/router';
 import { SlideModel } from 'src/app/core/models/slide.model';
 import SwiperCore, { Navigation, Pagination, Scrollbar, Virtual } from 'swiper';
 
@@ -13,4 +14,10 @@ SwiperCore.use([Virtual, Navigation, Pagination, Scrollbar]);
 })
 export class SliderComponent {
   @Input() items: SlideModel[];
+
+  constructor(private router: Router) { }
+
+  handleRedirect(url: string): void {
+    (window as any).open(url, '_blank');
+  }
 }
