6e795ef7a68dc31176068146323c528d562cdf32
brayangomez08 <gomezmancobrayanalexander@gmail.com>
2022-05-24 01:45:07 -0500

    Changed slider styles to be responsive

--- src/app/ui/blocks/slider/slider.component.ts
+++ src/app/ui/blocks/slider/slider.component.ts
@@ -1,9 +1,9 @@
 import { Component, ChangeDetectionStrategy, Input, ViewEncapsulation } from '@angular/core';
 import { Router } from '@angular/router';
 import { SlideModel } from 'src/app/core/models/slide.model';
-import SwiperCore, { Navigation, Pagination, Scrollbar, Virtual } from 'swiper';
+import SwiperCore, { Navigation, Pagination, Scrollbar, Virtual, Autoplay, EffectFade } from 'swiper';
 
-SwiperCore.use([Virtual, Navigation, Pagination, Scrollbar]);
+SwiperCore.use([Virtual, Navigation, Pagination, Scrollbar, Autoplay, EffectFade]);
 
 @Component({
   selector: 'mr-slider',
